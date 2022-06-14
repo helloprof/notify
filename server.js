aa36d440af67aa4cb92f2c1a372cff8474f6f0a8
helloprof <suluxan.seneca@gmail.com>
2022-06-14 11:03:01 -0400

    cloudinary set up with post request routes and id routes

--- server.js
+++ server.js
@@ -5,8 +5,19 @@ const env = require("dotenv")
 env.config()
 
 const path = require("path")
-
 const musicService = require("./musicService")
+
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+  secure: true
+})
+
 
 const HTTP_PORT = process.env.PORT || 8080
 
@@ -16,8 +27,10 @@ function onHttpStart() {
 
 app.use(express.static("public"));
 
+const upload = multer(); // no { storage: storage } since we are not using disk storage
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname,"/views/index.html"))
+  res.sendFile(path.join(__dirname, "/views/index.html"))
 })
 
 app.get("/albums", (req, res) => {
@@ -30,6 +43,58 @@ app.get("/albums", (req, res) => {
   // what the final response should look like: res.json(albumsData)
 })
 
+app.get("/albums/new", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/albumForm.html"))
+})
+
+app.get("/albums/:id", (req, res) => {
+  musicService.getAlbumById(req.params.id).then((album) => {
+    res.json(album)
+  }).catch((err) => {
+    res.json({ message: err })
+  })
+})
+
+app.post("/albums/new", upload.single("albumCover"), (req, res) => {
+  if (req.file) {
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream(
+          (error, result) => {
+            if (result) {
+              resolve(result);
+            } else {
+              reject(error);
+            }
+          }
+        );
+
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+
+    async function upload(req) {
+      let result = await streamUpload(req);
+      console.log(result);
+      return result;
+    }
+
+    upload(req).then((uploaded) => {
+      processPost(uploaded.url);
+    });
+  } else {
+    processPost("");
+  }
+
+  function processPost(imageUrl) {
+    req.body.albumCover = imageUrl;
+
+    // TODO: Process the req.body and add it as a new Blog Post before redirecting to /posts
+  }
+
+})
+
+
 app.get("/genres", (req, res) => {
   musicService.getGenres().then((data) => {
     res.json(data)
@@ -39,6 +104,8 @@ app.get("/genres", (req, res) => {
 
   // what the final response should look like: res.json(albumsData)
 })
+
+
 
 app.use((req, res) => {
   res.status(404).send("Page Not Found")
