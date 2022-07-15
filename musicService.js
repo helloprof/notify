a5c883be5df4fc043534c394dc229abcc800f463
helloprof <suluxan.seneca@gmail.com>
2022-07-15 14:35:36 -0400

    added Album, Song, and Genre models for postgres and also connected to our postgres server

--- musicService.js
+++ musicService.js
@@ -3,6 +3,77 @@ const fs = require("fs")
 let albums = []
 let genres = []
 
+const env = require("dotenv")
+env.config()
+
+const Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: 5432,
+    dialectOptions: {
+        ssl: { rejectUnauthorized: false }
+    },
+    query: { raw: true }
+})
+
+// // just to check if postgres credentials are working
+// sequelize
+//     .authenticate()
+//     .then(function() {
+//         console.log('Connection has been established successfully.')
+//     })
+//     .catch(function(err) {
+//         console.log('Unable to connect to the database:', err);
+//     })
+
+
+
+var object = {"id": 1,
+              "title": "Graduation",
+              "artist": "Kanye West",
+              "year": "2007",
+              "albumCover": "https://a5.mzstatic.com/us/r1000/0/Music114/v4/db/ce/e7/dbcee73f-015e-d994-abe4-58fbdfd0569c/00602577027864.rgb.jpg",
+              "genre":"Hip-Hop",
+              "singleSong": "Stronger"}
+
+var Album = sequelize.define('Album', {
+  albumID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  }, 
+  title: Sequelize.STRING,
+  artist: Sequelize.STRING,
+  year: Sequelize.INTEGER,
+  albumCover: Sequelize.STRING
+})
+
+var Song = sequelize.define('Song', {
+  songID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  }, 
+  title: Sequelize.STRING,
+  songFile: Sequelize.STRING,
+  single: Sequelize.BOOLEAN
+})
+
+var Genre = sequelize.define('Genre', {
+  genreID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  }, 
+  genre: Sequelize.STRING
+})
+
+Song.belongsTo(Album, {foreignKey: 'albumID'})
+Album.belongsTo(Genre, {foreignKey: 'genreID'})
+
+
 module.exports.initialize = () => {
     return new Promise((resolve, reject) => {
         fs.readFile('./data/albums.json', 'utf8', (err, data) => {
